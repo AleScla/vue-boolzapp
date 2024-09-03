be15611045b3f1f4fe84fd6a06e686737c598020
AleScla <scla.alessandro@gmail.com>
2024-09-03 19:42:20 +0200

    completato markup statico aggiunto elementi nelle anteprime dinamicamente

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,24 @@ createApp({
         contacts: [
             {
                 name: 'Michele',
-                avatar: './img/avatar_1.png',
+                avatar: './img/avatar_1.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour: '15:35',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
+                        date: '10/01/2020',
+                        hour:'16:15',
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
@@ -28,21 +31,24 @@ createApp({
             },
             {
                 name: 'Fabio',
-                avatar: './img/avatar_2.png',
+                avatar: './img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
+                        date: '20/03/2020',
+                        hour:'16:30',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '20/03/2020',
+                        hour:'16:30',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '20/03/2020',
+                        hour:'16:35',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     }
@@ -50,21 +56,24 @@ createApp({
             },
             {
                 name: 'Samuele',
-                avatar: './img/avatar_3.png',
+                avatar: './img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
+                        date: '28/03/2020',
+                        hour:'10:10',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '28/03/2020',
+                        hour:'10:20',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: '28/03/2020',
+                        hour:'16:15',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -72,16 +81,18 @@ createApp({
             },
             {
                 name: 'Alessandro B.',
-                avatar: './img/avatar_4.png',
+                avatar: './img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour:'15:30',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
@@ -89,16 +100,18 @@ createApp({
             },
             {
                 name: 'Alessandro L.',
-                avatar: './img/avatar_5.png',
+                avatar: './img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour:'15:30',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
                     }
@@ -106,21 +119,24 @@ createApp({
             },
             {
                 name: 'Claudia',
-                avatar: './img/avatar_6.png',
+                avatar: './img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour:'15:30',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '10/01/2020',
+                        hour:'15:51',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                     }
@@ -128,16 +144,18 @@ createApp({
             },
             {
                 name: 'Federico',
-                avatar: './img/avatar_7.png',
+                avatar: './img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour:'15:30',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                     }
@@ -145,28 +163,31 @@ createApp({
             },
             {
                 name: 'Davide',
-                avatar: './img/avatar_8.png',
+                avatar: './img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        hour:'15:30',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        hour:'15:50',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '10/01/2020',
+                        hour:'15:51',
                         message: 'OK!!',
                         status: 'received'
                     }
                 ],
             }
-        ]
-        
+        ],
+        active:0,
     }
   }
 }).mount('#app')
